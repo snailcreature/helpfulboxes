e4088f31989d6e50f93db24eb9ffe1372270a0e3
SDCard <sam.drage@yahoo.com>
2020-01-20 22:40:44 +0000

    got linked blocks working

--- src/main/java/com/sam/helpfulboxes/common/block/box/HopBox.java
+++ src/main/java/com/sam/helpfulboxes/common/block/box/HopBox.java
@@ -2,16 +2,25 @@ package com.sam.helpfulboxes.common.block.box;
 
 import com.sam.helpfulboxes.common.lib.Dictionary;
 import com.sam.helpfulboxes.common.block.BlockMod;
+import com.sam.helpfulboxes.common.lib.TagDict;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.dimension.Dimension;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
+
+import javax.annotation.Nullable;
 
 
 public class HopBox extends BlockMod {
 
     private BlockPos linkPos = null;
-    private Dimension linkDim = null;
+    private DimensionType linkDim = null;
+    private BlockPos thisPos;
+    private DimensionType thisDim;
 
     public HopBox()    {
         super(Dictionary.Block.HOP_BOX, Block.Properties.create(Material.WOOD));
@@ -20,13 +29,35 @@ public class HopBox extends BlockMod {
     public BlockPos getLinkPos()    {
         return linkPos;
     }
-
-    public Dimension getLinkDim()   {
+    public DimensionType getLinkDim()   {
         return linkDim;
+    }
+
+    public boolean setLink(BlockPos pos, DimensionType dim, World world) {
+        linkPos = pos;
+        linkDim = dim;
+
+        Block linkedBlock = world.getBlockState(pos).getBlock();
+        if (linkedBlock.getTags().contains(TagDict.Blocks.HOP_BOX)) {
+            return ((HopBox) linkedBlock).setLink(thisPos, thisDim);
+        }
+        return false;
+    }
+
+    public boolean setLink(BlockPos pos, DimensionType dim)    {
+        linkPos = pos;
+        linkDim = dim;
+        return true;
     }
 
     public boolean isWritten()  {
         return linkPos != null;
     }
 
+    @Override
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
+        thisPos = pos;
+        thisDim = worldIn.getDimension().getType();
+        super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
+    }
 }
--- src/main/java/com/sam/helpfulboxes/common/item/tool/LinkStone.java
+++ src/main/java/com/sam/helpfulboxes/common/item/tool/LinkStone.java
@@ -1,5 +1,6 @@
 package com.sam.helpfulboxes.common.item.tool;
 
+import com.sam.helpfulboxes.common.block.box.HopBox;
 import com.sam.helpfulboxes.common.lib.Dictionary;
 import com.sam.helpfulboxes.common.item.ItemMod;
 import com.sam.helpfulboxes.common.lib.TagDict;
@@ -8,12 +9,12 @@ import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.dimension.Dimension;
+import net.minecraft.world.dimension.DimensionType;
 
 public class LinkStone extends ItemMod {
 
     private BlockPos linkPos = null;
-    private Dimension linkDim = null;
+    private DimensionType linkDim = null;
 
     public LinkStone() {
         super(Dictionary.Item.LINK_STONE, new Properties());
@@ -27,7 +28,21 @@ public class LinkStone extends ItemMod {
         Block block = world.getBlockState(pos).getBlock();
 
         if (block.getTags().contains(TagDict.Blocks.HOP_BOX))   {
-            ;
+            if (isWritten()) {
+                if (!((HopBox) block).isWritten()) {
+                    Boolean check = ((HopBox) block).setLink(linkPos, linkDim, world);
+                    if (check) {
+                        return ActionResultType.SUCCESS;
+                    }
+                    return ActionResultType.FAIL;
+                }
+            }
+            else    {
+                if (((HopBox) block).isWritten())   {
+                    linkPos = pos;
+                    linkDim = world.getDimension().getType();
+                }
+            }
         }
 
         return super.onItemUse(context);
